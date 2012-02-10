5830490fad132c916a7b3b3ad0bef91f63a491b6
Simon Gomizelj <simongmzlj@gmail.com>
2012-02-10 02:04:06 -0500

    More explicit typing.

--- Alpm.hs
+++ Alpm.hs
@@ -48,7 +48,7 @@ defaultOptions = AlpmOptions
 foreign import ccall "alpm_strerror" c_alpm_strerror :: CInt -> CString
 alpmStrerror errno = unsafePerformIO . peekCString $ c_alpm_strerror errno
 
-foreign import ccall "alpm_initialize" c_alpm_initialize :: CString -> CString -> Ptr a -> IO (Ptr b)
+foreign import ccall "alpm_initialize" c_alpm_initialize :: CString -> CString -> Ptr CInt -> IO (Ptr AlpmHandle)
 alpmInitialize :: AlpmOptions -> IO AlpmConf
 alpmInitialize opt = do
     root'   <- newCString $ root opt
@@ -59,12 +59,12 @@ alpmInitialize opt = do
             then peek errPtr >>= fail . printf "failed to initialize alpm library (%s)" . alpmStrerror
             else flip AlpmConf opt <$> newForeignPtr c_alpm_release alpm_ptr
 
-foreign import ccall "&alpm_release" c_alpm_release :: FinalizerPtr a
+foreign import ccall "&alpm_release" c_alpm_release :: FinalizerPtr AlpmHandle
 
 withAlpmPtr :: (Ptr AlpmHandle -> IO b) -> Alpm b
 withAlpmPtr f = asks alpmPtr >>= \a -> liftIO $ withForeignPtr a f
 
-foreign import ccall "alpm_option_get_localdb" c_alpm_option_get_localdb :: Ptr a -> IO (Ptr b)
+foreign import ccall "alpm_option_get_localdb" c_alpm_option_get_localdb :: Ptr AlpmHandle -> IO (Ptr AlpmList)
 localDB :: Alpm DB
 localDB = withAlpmPtr $ \alpm_ptr -> do
     db_ptr <- c_alpm_option_get_localdb alpm_ptr
@@ -72,14 +72,14 @@ localDB = withAlpmPtr $ \alpm_ptr -> do
         then fail "could not register 'local' database"
         else return $ DB db_ptr
 
-foreign import ccall "alpm_db_get_pkgcache" c_alpm_db_get_pkgcache :: Ptr a -> Ptr b
-foreign import ccall "alpm_list_next"       c_alpm_list_next       :: Ptr a -> Ptr b
-foreign import ccall "alpm_list_getdata"    c_alpm_list_getdata    :: Ptr a -> Ptr b
+foreign import ccall "alpm_db_get_pkgcache" c_alpm_db_get_pkgcache :: Ptr AlpmList -> Ptr AlpmList
+foreign import ccall "alpm_list_next"       c_alpm_list_next       :: Ptr AlpmList -> Ptr AlpmList
+foreign import ccall "alpm_list_getdata"    c_alpm_list_getdata    :: Ptr AlpmList -> Ptr b
 
 packages :: DB -> [Package]
 packages (DB db_ptr) = integrate (Package . c_alpm_list_getdata) $ c_alpm_db_get_pkgcache db_ptr
 
-integrate :: (Ptr a -> b) -> Ptr a -> [b]
+integrate :: (Ptr AlpmList -> b) -> Ptr AlpmList -> [b]
 integrate box ptr
     | isNull ptr = []
     | otherwise  = box ptr : integrate box (c_alpm_list_next ptr)
--- Alpm/Package.hs
+++ Alpm/Package.hs
@@ -16,14 +16,14 @@ data Package = Package (Ptr PkgHandle)
 unsafePeekCString :: CString -> String
 unsafePeekCString = unsafePerformIO . peekCString
 
-foreign import ccall "alpm_pkg_get_name"      c_alpm_get_name      :: Ptr a -> CString
-foreign import ccall "alpm_pkg_get_version"   c_alpm_get_version   :: Ptr a -> CString
-foreign import ccall "alpm_pkg_get_desc"      c_alpm_get_desc      :: Ptr a -> CString
-foreign import ccall "alpm_pkg_get_url"       c_alpm_get_url       :: Ptr a -> CString
-foreign import ccall "alpm_pkg_get_packager"  c_alpm_get_packager  :: Ptr a -> CString
-foreign import ccall "alpm_pkg_get_arch"      c_alpm_get_arch      :: Ptr a -> CString
+foreign import ccall "alpm_pkg_get_name"     c_alpm_get_name     :: Ptr PkgHandle -> CString
+foreign import ccall "alpm_pkg_get_version"  c_alpm_get_version  :: Ptr PkgHandle -> CString
+foreign import ccall "alpm_pkg_get_desc"     c_alpm_get_desc     :: Ptr PkgHandle -> CString
+foreign import ccall "alpm_pkg_get_url"      c_alpm_get_url      :: Ptr PkgHandle -> CString
+foreign import ccall "alpm_pkg_get_packager" c_alpm_get_packager :: Ptr PkgHandle -> CString
+foreign import ccall "alpm_pkg_get_arch"     c_alpm_get_arch     :: Ptr PkgHandle -> CString
 
-foreign import ccall "alpm_pkg_get_size"      c_alpm_get_size      :: Ptr a -> CSize
+foreign import ccall "alpm_pkg_get_size"     c_alpm_get_size     :: Ptr PkgHandle -> CSize
 
 packageName :: Package -> String
 packageName (Package pkg_ptr) = unsafePeekCString $ c_alpm_get_name pkg_ptr
