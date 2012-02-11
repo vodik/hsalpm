e5126750ee196f077a5fb8515f4588ee191da4b0
Simon Gomizelj <simongmzlj@gmail.com>
2012-02-11 18:20:39 -0500

    No point.

--- Main.hs
+++ Main.hs
@@ -3,9 +3,9 @@ module Main where
 import Control.Applicative
 import Control.Monad.Trans (liftIO)
 import Data.Char
+import Data.Foldable (forM_)
 import Data.List
 import System.Environment
-import qualified Data.Foldable as F
 
 import Alpm
 import Alpm.Database
@@ -17,16 +17,16 @@ main = do
     pkgs <- runAlpm options $ do
         local <- packages <$> localDB
         return $ filter (myFilter args) local
-    F.forM_ pkgs ppPkgInfo
+    forM_ pkgs ppPkgInfo
   where
     options = defaultOptions
 
 myFilter :: [String] -> Package -> Bool
 myFilter ts pkg =
     let ts' = map (map toLower) ts
-        f1  = F.all (`isInfixOf` packageName pkg) ts'
-        f2  = F.all (`isInfixOf` map toLower (packageDescription pkg)) ts'
-        f3  = F.any (`F.elem` packageGroups pkg) ts'
+        f1  = all (`isInfixOf` packageName pkg) ts'
+        f2  = all (`isInfixOf` map toLower (packageDescription pkg)) ts'
+        f3  = any (`elem` packageGroups pkg) ts'
     in f1 || f2 || f3
 
 ppPkgInfo :: Package -> IO ()
