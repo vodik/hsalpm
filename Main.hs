67c6e993524c6bb77019b569433fd9cabf29e286
Simon Gomizelj <simongmzlj@gmail.com>
2012-02-11 18:14:25 -0500

    Play around with foldable.

--- Main.hs
+++ Main.hs
@@ -2,9 +2,10 @@ module Main where
 
 import Control.Applicative
 import Control.Monad.Trans (liftIO)
-import Data.List
 import Data.Char
+import Data.List
 import System.Environment
+import qualified Data.Foldable as F
 
 import Alpm
 import Alpm.Database
@@ -16,16 +17,16 @@ main = do
     pkgs <- runAlpm options $ do
         local <- packages <$> localDB
         return $ filter (myFilter args) local
-    mapM_ ppPkgInfo pkgs
+    F.forM_ pkgs ppPkgInfo
   where
     options = defaultOptions
 
 myFilter :: [String] -> Package -> Bool
 myFilter ts pkg =
     let ts' = map (map toLower) ts
-        f1  = all (`isInfixOf` packageName pkg) ts'
-        f2  = all (`isInfixOf` map toLower (packageDescription pkg)) ts'
-        f3  = any (`elem` packageGroups pkg) ts'
+        f1  = F.all (`isInfixOf` packageName pkg) ts'
+        f2  = F.all (`isInfixOf` map toLower (packageDescription pkg)) ts'
+        f3  = F.any (`F.elem` packageGroups pkg) ts'
     in f1 || f2 || f3
 
 ppPkgInfo :: Package -> IO ()
