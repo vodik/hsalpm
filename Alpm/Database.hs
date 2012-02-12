f32be1a477f5c8ce3555aa144d6977b4da98838d
Simon Gomizelj <simongmzlj@gmail.com>
2012-02-11 19:34:28 -0500

    Added ability to access synced databases.

--- Alpm/Database.hs
+++ Alpm/Database.hs
@@ -11,9 +11,11 @@ import Alpm.List
 import Alpm.Package
 import Alpm.Util
 
-data DB = DB !(Ptr AlpmList)
+data DBHandle
 
-foreign import ccall "alpm_option_get_localdb" c_alpm_option_get_localdb :: Ptr AlpmHandle -> IO (Ptr AlpmList)
+data DB = DB !(Ptr DBHandle)
+
+foreign import ccall "alpm_option_get_localdb" c_alpm_option_get_localdb :: Ptr AlpmHandle -> IO (Ptr DBHandle)
 localDB :: Alpm DB
 localDB = withAlpmPtr $ \alpm_ptr -> do
     db_ptr <- c_alpm_option_get_localdb alpm_ptr
@@ -21,6 +23,23 @@ localDB = withAlpmPtr $ \alpm_ptr -> do
         then fail "could not register 'local' database"
         else return $ DB db_ptr
 
-foreign import ccall "alpm_db_get_pkgcache" c_alpm_db_get_pkgcache :: Ptr AlpmList -> Ptr AlpmList
+foreign import ccall "alpm_option_get_syncdbs" c_alpm_option_get_syncdbs :: Ptr AlpmHandle -> IO (Ptr AlpmList)
+syncDBs :: Alpm [DB]
+syncDBs = withAlpmPtr $ \alpm_ptr -> do
+    list_ptr <- c_alpm_option_get_syncdbs alpm_ptr
+    if isNull list_ptr
+        then return []
+        else return . integrate DB $ list_ptr
+
+-- foreign input ccall "alpm_db_register_sync" c_alpm_db_register_sync :: Ptr AlpmHandle -> CString -> IO (Ptr DBHandle)
+-- syncDB :: String -> Alpm DB
+-- syncDB name = withAlpmPtr $ \alpm_ptr -> do
+--     name'  <- newCString name
+--     db_ptr <- c_alpm_db_register_sync alpm_ptr name'
+--     if isNull db_ptr
+--         then fail "could not register 'local' database"
+--         else return $ DB db_ptr
+
+foreign import ccall "alpm_db_get_pkgcache" c_alpm_db_get_pkgcache :: Ptr DBHandle -> Ptr AlpmList
 packages :: DB -> [Package]
 packages (DB db_ptr) = integrate mkPackage $ c_alpm_db_get_pkgcache db_ptr
