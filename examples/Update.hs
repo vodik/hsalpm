317716acbd3e85289e1a1c359766c611bbe7a612
Simon Gomizelj <simongmzlj@gmail.com>
2012-02-14 01:28:45 -0500

    Play around with callbacks.

--- examples/Update.hs
+++ examples/Update.hs
@@ -1,15 +1,19 @@
 module Main where
 
 import Control.Monad
+import Foreign.C.String
 
 import Alpm
 import Alpm.Database
 import Alpm.Package
+import Alpm.Option
 import Pacman
 
 main :: IO ()
 main = do
     conf <- getPacman
     runAlpm defaultOptions $ do
-        dbs  <- pacmanDBs conf
-        forM_ dbs $ updateDB False
+        setProgressCB $ \_ str _ _ _ -> do
+            peekCString str >>= putStrLn
+
+        pacmanDBs conf >>= mapM_ (updateDB False)
